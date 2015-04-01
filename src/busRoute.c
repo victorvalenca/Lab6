a7e3f4dc325a8f749420ea2a2138d4523f2edf8f
Victor Fernandes <victorvalenca@gmail.com>
2015-04-01 14:57:01 -0700

    Initial code skeleton

---
+++ src/busRoute.c
@@ -0,0 +1,30 @@
+/**
+ * Lab 6: Using an Array of Structures
+ * Author: Victor Fernandes
+ * Purpose: To add/remove bus routes from an array
+ **/
+
+/* Include statements **/
+#include <stdio.h>
+#include "busRoute.h"
+
+/** Forward declarations and variables **/
+void getBusRouteInfo(int);
+void getNextLowestIdx();
+void removeRoute(int);
+void setBusRouteInfo();
+
+static busRoute routes[1000];
+static lowestIdx;
+
+int main (){
+	// TODO: Initalize variables
+	routes = {0};
+	lowestIdx = 0;
+
+	while(1){
+	//TODO: Display Menu
+	//TODO: Write forward-declared functions
+	}
+
+}
---
+++ src/busRoute.h
@@ -0,0 +1,8 @@
+struct busRoute{
+	int busRouteNumber;
+	char *startingLocation;
+	char *endingLocation;
+	char driverName[36];
+}
+
+typedef struct busRoute busRoute;
